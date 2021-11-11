c624c52822ac349930529c094afc72c44ffefcbc
Abdisatar Mohamed <asatar1994@gmail.com>
2021-11-11 01:18:31 -0800

    changed to purified css

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ a recruitment agency," />
 
 
   <!-- Argon CSS -->
-  <link type="text/css" href="assets/css/argon.min.css" rel="stylesheet preconnect" />
+  <link type="text/css" href="assets/css/argon-purified-min.css" rel="stylesheet" />
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <style>
     .card-whwd {
